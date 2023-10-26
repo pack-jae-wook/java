8c2e6333b5ec75ddc251669e2747469aa6951653
박재욱 <74747556+park-jae-wook@users.noreply.github.com>
2023-10-27 08:32:16 +0900

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,23 @@
+package javastudy;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		
+		//주석 : 코드로 수행하지 않고 설명을 붙여놓은 것처럼 처리한다.
+		//main()함수 : 프로그램의 처리 시작점이다.!!!
+		//화면에 출력 : System.out.println();
+		//처리해야 하는 하나의 구문 마지막에는 세미콜론(;)을 반드시 붙여준다.
+		//Ctrl + Space : :명령어 자동 완성 기능이다.
+		//Ctrl + F11 : 컴파일과 수행을 동시에 !!!
+		
+       System.out.println("Hello Java!!!");
+       System.out.println("안녕하세요!!!");
+       //이름, 나이, 키, 혈액형, 코딩여부
+       System.out.println("이름은 박재욱 입니다.");
+       System.out.println("나이는 23세 입니다.");
+       System.out.println("키는 169cm 입니다.");
+       System.out.println("혈액형은 A형 입니다.");
+       System.out.println("자바 프로그램 코딩은 할 수 있습니다.");
+	}
+}
